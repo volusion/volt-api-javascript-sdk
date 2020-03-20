510981b19d2b72a219d1f51d88904662b8a326ff
Kevin Sproles <kevinsproles@users.noreply.github.com>
2020-03-20 12:23:17 -0500

    Updated README.md

---
+++ readme.md
@@ -0,0 +1,50 @@
+# volt api client for nodejs
+
+Easily integrate with the [Volt by Volusion](https://volusion.com/volt) API. This is the core ecommerce data of a store such as Products, Categories, Orders. See [Volt API Documentation](https://app.swaggerhub.com/apis-docs/volusion/VOLT). 
+
+If you're developing a theme, check out the [Element Docs](https://volusion.github.io/element/) instead.
+
+## Install
+
+Use this package in your project by doing:
+
+```bash
+npm install @volusion/volt-api
+```
+
+
+## Get Started
+
+```js
+// instantiate the client 
+const voltClient = require('@volusion/volt-api');
+const volt = new voltClient({
+    apiKey: "9fduAnbishA0N9BXlNQ6zC:9kaub81gaVl1oJfXiIbal8" // replace with your key
+});
+
+// call an endpoint
+const data = await volt.getOrders();
+
+// use the data
+const order = data.items[0]; // for this sample, let's look at the first order in the array
+
+```
+
+## Security Tip
+
+We recommend setting an environment variable in your project called `VOLT_API_KEY`.
+
+You probably already know how to do this using popular packages like [dotenv](https://www.npmjs.com/package/dotenv) or similar to manage environment variables. But if not, you can quickly set your environment variables locally by simply running the following command which sets the environment variable for the duration of your terminal session. So just run this before you run `npm start`.
+
+
+```js
+// when you instantiate the client, get the key from an environment variable
+const voltClient = require('@volusion/volt-api');
+const volt = new voltClient({
+    apiKey: process.env.VOLT_API_KEY // 👈 best practice ✨
+});
+```
+
+## Future
+
+- more examples
---
+++ volt.js
@@ -0,0 +1,73 @@
+const axios = require('axios');
+const querystring = require('querystring');
+const baseUrl = "https://volt-api.volusion.com"
+
+module.exports = class voltApi {
+
+    constructor(config) {
+        this.config = config;
+    }
+
+    getOrders = async function getOrders(options) {
+        return await this.makeVoltApiRequest({method: "GET", url: `/store/admin/orders` + otq(options)})
+        // todo: make sure we support updatedAfter. Also need to handle paging or just return a boat load 🚢.
+    }
+    updateOrder = async function updateOrder(id, data, options) {
+        return await this.makeVoltApiRequest({method: "POST", url: `/store/admin/orders/${id}` + otq(options), data: data})
+    }
+
+    makeVoltApiRequest = async function makeVoltApiRequest(r) {
+        try {
+            const { data } = await axios({
+                headers: {
+                    "Authorization": this.config.apiKey,
+                    "Content-Type": "application/json"
+                },
+                url: baseUrl + r.url,
+                method: r.method,
+                data: r.body || null
+            });
+            return data;
+        } catch (error) {
+            handleError(error);
+            throw error;
+        }
+    }
+
+}
+
+// ----------------------------------------------------
+// Helper Functions
+// ----------------------------------------------------
+
+function otq(options) {
+    
+    // turns something like this:
+    // {
+    //     pageSize: 10,
+    //     foo: bar    
+    // }
+    // into this:
+    // "?pageSize=10&foo=bar"
+
+    let qs = querystring.stringify(options);
+    if (qs) {
+        qs = "?" + qs
+    }
+    return qs;
+
+}
+
+function handleError(error) {
+    if (error.response) {
+        // The request was made and the server responded with a status code that falls out of the range of 2xx
+        console.log("error.response.data", error.response.data);
+        console.log("error.response.status", error.response.status);
+    } else if (error.request) {
+        // The request was made but no response was received
+        console.log("error.request", error.request);
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("error.message", error.message);
+    }
+}
